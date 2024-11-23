382dcb16669698e52555f4eca7fc2976bde24657
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-11-23 22:32:47 +0100

    Changed shadcn components to no alias imports

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "../../src/lib/utils";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 const Sheet = SheetPrimitive.Root;
